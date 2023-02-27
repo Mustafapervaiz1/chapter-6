40397b69bf591c10f45f6e1fce7953f9495c86b2
mustafapervez <syalani.training1@gmail.com>
2023-02-27 23:39:26 +0500

    step 3 done

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ document.write(`The value of a is ${a} <br>`)
 document.write(".............................<br>")
 document.write(`The value of ++a is${++a} <br>`)
 document.write(`Now value of a is${a} <br>`)
+// step 3
+document.write(`The value of a++ is${a++} <br>`)
+document.write(`Now value of a is${a} <br>`)

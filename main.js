1abf267440c173acf84d226c907c5d291a051b12
mustafapervez <syalani.training1@gmail.com>
2023-02-28 00:06:30 +0500

    question 2 completed

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-document.write("Result:<br>")
-var a = 10
-document.write(`The value of a is ${a} <br>`)
-document.write(".............................<br><br>")
-document.write(`The value of ++a is${++a} <br>`)
-document.write(`Now value of a is${a} <br><br>`)
-// step 3
-document.write(`The value of a++ is${a++} <br>`)
-document.write(`Now value of a is${a} <br><br>`)
-// step 4
-document.write(`The value of --a is${--a} <br>`)
-document.write(`Now value of a is${a} <br><br>`)
-// step 5
-document.write(`The value of a-- is${a--} <br>`)
-document.write(`Now value of a is${a} <br>`)
+// document.write("Result:<br>")
+// var a = 10
+// document.write(`The value of a is ${a} <br>`)
+// document.write(".............................<br><br>")
+// document.write(`The value of ++a is${++a} <br>`)
+// document.write(`Now value of a is${a} <br><br>`)
+// // step 3
+// document.write(`The value of a++ is${a++} <br>`)
+// document.write(`Now value of a is${a} <br><br>`)
+// // step 4
+// document.write(`The value of --a is${--a} <br>`)
+// document.write(`Now value of a is${a} <br><br>`)
+// // step 5
+// document.write(`The value of a-- is${a--} <br>`)
+// document.write(`Now value of a is${a} <br>`)
 // step 2.1 
 var a =2,b=1;
+
+document.write(`a is ${a}<br>`)
+document.write(`b is ${b}<br>`)
+document.write(`result  is ${--a - --b + ++b + b--} <br>`)

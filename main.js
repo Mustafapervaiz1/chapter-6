7d30d7b6f57012c5544e93262063d0d69c77c24d
mustafapervez <syalani.training1@gmail.com>
2023-02-27 23:43:46 +0500

    final step

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 document.write("Result:<br>")
 var a = 10
 document.write(`The value of a is ${a} <br>`)
-document.write(".............................<br>")
+document.write(".............................<br><br>")
 document.write(`The value of ++a is${++a} <br>`)
-document.write(`Now value of a is${a} <br>`)
+document.write(`Now value of a is${a} <br><br>`)
 // step 3
 document.write(`The value of a++ is${a++} <br>`)
-document.write(`Now value of a is${a} <br>`)
+document.write(`Now value of a is${a} <br><br>`)
 // step 4
 document.write(`The value of --a is${--a} <br>`)
+document.write(`Now value of a is${a} <br><br>`)
+// step 5
+document.write(`The value of a-- is${a--} <br>`)
 document.write(`Now value of a is${a} <br>`)

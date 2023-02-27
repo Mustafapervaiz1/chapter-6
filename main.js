60442aa83a0acc013f8d8331458742b5d26a026f
mustafapervez <syalani.training1@gmail.com>
2023-02-27 23:38:10 +0500

    step 2 done

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ document.write("Result:<br>")
 var a = 10
 document.write(`The value of a is ${a} <br>`)
 document.write(".............................<br>")
+document.write(`The value of ++a is${++a} <br>`)
+document.write(`Now value of a is${a} <br>`)
